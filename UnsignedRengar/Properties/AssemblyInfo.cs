ce5c1273a1f8c6532deabfb8600d680491fbb678
4kupaas <discioglucagatay@gmail.com>
2017-03-02 05:59:37 +0200

    Add files via upload

--- UnsignedRengar/Properties/AssemblyInfo.cs
+++ UnsignedRengar/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unsigned Rengar")]
-[assembly: AssemblyDescription("Unsigned Series - Rengar - by Chaos")]
+[assembly: AssemblyTitle("ElRengarDecentralized")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EloBuddy")]
-[assembly: AssemblyProduct("Unsigned Rengar")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElRengarDecentralized")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9d36de17-2428-4a99-8397-5dfa9f7b7e5a")]
+[assembly: Guid("f75eabbe-b9e5-4627-b00c-c9d81642c1d6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
